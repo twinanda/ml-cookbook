4860ba7ac658cff809c689dcf27c546ed21a052d
Cyril K <69794369+cyril-k@users.noreply.github.com>
2025-05-21 10:45:47 +0200

    Update setup.sh

--- slurm/hf-accelerate/setup.sh
+++ slurm/hf-accelerate/setup.sh
@@ -8,3 +8,8 @@ pip install -r requirements.txt
 
 wget https://raw.githubusercontent.com/huggingface/accelerate/refs/heads/main/examples/slurm/fsdp_config.yaml
 wget https://raw.githubusercontent.com/huggingface/accelerate/refs/heads/main/examples/complete_nlp_example.py
+
+# download, dataset, metric and and model to cache
+python -c "from datasets import load_dataset; load_dataset('glue', 'mrpc')" 
+python -c "import evaluate; evaluate.load('glue', 'mrpc')"
+python -c "from transformers import pipeline; pipeline('fill-mask', model='bert-base-cased')"
