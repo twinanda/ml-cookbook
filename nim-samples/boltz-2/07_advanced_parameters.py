e6e9270f94150998fd368a1e552486947773090d
Arseniy Sokolov <sokolov.ars.a@nebius.com>
2025-09-18 21:14:26 +0200

    fix: comment out `without_potentials` argument in calling `client.predict_protein_structure`
    - Method `predict_protein_structure` in `Boltz2Client` doesn't set `without_potentials` field during `PredictionRequest` pydantic model creation.
    - So that's bug on NVIDIA side again.
    - For test/demonstration purposes we can omit this argument.

--- nim-samples/boltz-2/07_advanced_parameters.py
+++ nim-samples/boltz-2/07_advanced_parameters.py
@@ -314,7 +314,7 @@ async def specialized_options():
             result = await client.predict_protein_structure(
                 sequence=sequence,
                 polymer_id="A",
-                without_potentials=option.get('without_potentials', False),
+                #without_potentials=option.get('without_potentials', False),
                 step_scale=option.get('step_scale', 1.638),
                 recycling_steps=option['recycling_steps'],
                 sampling_steps=option['sampling_steps']
