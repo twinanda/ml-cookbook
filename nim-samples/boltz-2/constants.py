aa6154673c00f26af4074fc704547a55593d51c7
Andru Twinanda <andru@nebius.com>
2025-10-27 15:59:12 +0100

    Cleaned up Boltz2 examples

--- nim-samples/boltz-2/constants.py
+++ nim-samples/boltz-2/constants.py
@@ -1,6 +1,6 @@
 # This file is added to help managing the URL to the deployed Boltz2 NIM
 
 host = '<host>' # TODO: Fill in with the deployed NIM URL, without http://, https:// and forward slashes
-username = '<host>' # TODO: Fill in with username from deployment
-password = '<host>' # TODO: Fill in with password from deployment
+username = '<username>' # TODO: Fill in with username from deployment
+password = '<password>' # TODO: Fill in with password from deployment
 base_url = f'https://{username}:{password}@{host}'
